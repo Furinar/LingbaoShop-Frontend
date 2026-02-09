cff8e5ace4918078f1227a62be52d1d554cc352c
Furina <3559551198@qq.com>
2026-02-09 20:26:46 +0800

    基本架构

---
+++ src/views/HomeView.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="container">
+    <div class="header">
+      <img src="/Lingbao-Shop.svg" alt="Lingbao-Shop">
+      灵宝集市
+    </div>
+
+    <div class="body">
+
+    </div>
+
+    <div class="footer">
+      CopyRight &copy; 2026 灵宝集市分享码
+
+      本站仅供学习交流使用，请勿用于商业用途
+
+      联系我们 2102733232@qq.com
+
+      Made by
+
+      Frontend Furina
+      Backend QiChenSn
+    </div>
+
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, reactive } from 'vue'
+
+
+</script>
+
+<style scoped>
+* {
+  padding: 0;
+  margin: 0;
+}
+
+.container {
+  width: 375px;
+  padding-left: auto;
+  padding-right: auto;
+}
+</style>
---
+++ vite.config.ts
@@ -0,0 +1,30 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueDevTools(),
+    AutoImport({
+      imports: ['vue', 'vue-router', 'pinia'],
+      dts: 'src/auto-imports.d.ts',
+      eslintrc: {
+        enabled: true,
+      },
+    }),
+    Components({
+      dts: 'src/components.d.ts',
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+  },
+})
